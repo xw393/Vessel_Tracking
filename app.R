69833a022f715bc06f3dd4b84d7c40fa327638fb
infinitecycle <wangxiao.stat@gmail.com>
2018-04-15 17:19:01 -0400

    add setting on directory address

--- app.R
+++ app.R
@@ -1,6 +1,10 @@
 library(shiny)
+library(rjson)
 
-setwd('~/Desktop/test_proj/map/')
+work_dir_config = fromJSON(file = "~/Desktop/workdir/Vessel_Tracking/configuration.json")
+
+setwd(work_dir_config$thinkpad_p51$work_dir)
+
 source('ui_map.R')
 source('server_map.R')
 
