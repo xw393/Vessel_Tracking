399de701076c3b6946d10ddb3f1c806df45f9173
infinitecycle <wangxiao.stat@gmail.com>
2018-04-16 10:07:58 -0400

    change work dir setting

--- app.R
+++ app.R
@@ -1,9 +1,10 @@
 library(shiny)
 library(rjson)
 
-work_dir_config = fromJSON(file = "~/Desktop/workdir/Vessel_Tracking/configuration.json")
+# work_dir_config = fromJSON(file = "~/Desktop/workdir/Vessel_Tracking/configuration.json")
 
-setwd(work_dir_config$thinkpad_p51$work_dir)
+work_dir = "~/Desktop/test_proj/map/Vessel_Tracking/" 
+setwd(work_dir)
 
 source('ui_map.R')
 source('server_map.R')
